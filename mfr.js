8d99129d401f7cd8e884da14f179a5798bb3703c
Ajay-kharat-007 <kharatajay023@gmail.com>
2023-01-01 14:38:30 +0530

    mfr

---
+++ mfr.js
@@ -0,0 +1,33 @@
+// let arr = [1,2,3,4,5,6,7,8,32,43,23,42,34,23,]
+
+// let map_arr = arr.map((elem, elem2)=>{ // value , index, array
+//     // return elem * elem
+//     // return elem >= 5
+//     console.log(`this is ${elem}, and this ${elem2}`)
+//     return elem + elem2
+
+// })
+
+// console.log(arr)
+// console.log(map_arr)
+
+
+// let fil_arr = arr.filter((elem)=>{
+//     return elem >= 5
+// })
+
+// console.log(arr)
+// console.log(fil_arr)
+
+// let red_arr = arr.reduce((elem , elem2)=>{
+//     console.log(`elem = ${elem} and elem2 = ${elem2}`)
+//     return elem + elem2
+// })
+
+// console.log(arr)
+// console.log(red_arr)
+
+
+// map  - we can target all elem one by one and can perform some action with them
+// filter - filter the array true values are retured (false value does not returns)
+// reduce - reduce reduce the array to one single value (to do action using all array elemets )
